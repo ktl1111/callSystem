0d96e633e62f9f937665b9821ecfa7071a5c6269
Vany.Liu <vany8462.fl00@gmail.com>
2022-04-06 14:38:00 +0800

    edit settings.py

--- callSystem/settings.py
+++ callSystem/settings.py
@@ -26,7 +26,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-#-e*2pk+3tbmki0egzl!bv(j*nzd@fzu8s-yuzw$=brhzk@3fg'
+SECRET_KEY = 'django-insecure-#-e*2pk+3tbmki0egzl!bv(j*nzd@fzu8s-yuzw$=brhzk@3fg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
