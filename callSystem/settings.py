19ec84e88e5f88e92e5eb60df240f94f861dba79
Vany.Liu <vany8462.fl00@gmail.com>
2022-04-06 10:38:04 +0800

    edit settings.py

--- callSystem/settings.py
+++ callSystem/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+# reload(sys)
+# sys.getdefaultencoding('utf-8')
 
 django_heroku.settings(locals())
