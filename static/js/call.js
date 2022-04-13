be9f08e3d034cdbc34664375e48c738404891d04
Vany.Liu <vany8462.fl00@gmail.com>
2022-04-13 11:44:02 +0800

    print screen width and height

--- static/js/call.js
+++ static/js/call.js
@@ -15,3 +15,9 @@
 //     setInterval(function(){
 //    window.location.reload();
 // },3000);
+
+window.screen.height; //screen height
+window.screen.width; //screen width
+
+//For example, 1024x768:
+alert(window.screen.width+"x"+window.screen.height);
